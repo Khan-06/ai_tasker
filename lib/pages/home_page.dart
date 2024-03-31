32f66578c49d03557ece4eb153a72273d0b3ca84
TheMR-777 <m.shahzad.ms72@gmail.com>
2024-03-31 23:41:06 +0500

    Home Screen: Second Category Set ✅

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,22 +13,114 @@ class HomePage extends StatelessWidget {
       headerText: 'Social',
       onTap: () {},
       homeCards: [
+        // Twitter
         HomeCardProvider(
           headerIcon: 'assets/icons/twitter.png',
           headerText: 'Twitter',
           headerDescription: 'Write tweet, or turn text into tweet',
           onTap: () {},
         ),
+
+        // LinkedIn
+        HomeCardProvider(
+          headerIcon: 'assets/icons/linkedin.png',
+          headerText: 'LinkedIn',
+          headerDescription: 'Post, to elevate you professional presence',
+          onTap: () {},
+        ),
+
+        // Instagram
+        HomeCardProvider(
+          headerIcon: 'assets/icons/instagram.png',
+          headerText: 'Instagram',
+          headerDescription: 'Write captions for Instagram posts',
+          onTap: () {},
+        ),
+
+        // TikTok
+        HomeCardProvider(
+          headerIcon: 'assets/icons/tiktok.png',
+          headerText: 'TikTok',
+          headerDescription: 'Create captions that speak to your audience',
+          onTap: () {},
+        ),
+
+        // Multimedia
+        HomeCardProvider(
+          headerIcon: 'assets/icons/multimedia.png',
+          headerText: 'Video Ideas',
+          headerDescription: 'Create new video ideas for your channel',
+          onTap: () {},
+        ),
+
+        // YouTube
+        HomeCardProvider(
+          headerIcon: 'assets/icons/youtube.png',
+          headerText: 'YouTube',
+          headerDescription: 'Create video titles and descriptions',
+          onTap: () {},
+        ),
+
+        // Facebook
         HomeCardProvider(
           headerIcon: 'assets/icons/facebook.png',
           headerText: 'Facebook',
           headerDescription: 'Post on your timeline or create a story',
           onTap: () {},
         ),
+      ],
+    ),
+
+    // Personal
+    CardCategoryProvider(
+      headerText: 'Personal',
+      onTap: () {},
+      homeCards: [
+        // Birthday
         HomeCardProvider(
-          headerIcon: 'assets/icons/instagram.png',
-          headerText: 'Instagram',
-          headerDescription: 'Share a photo or a video',
+          headerIcon: 'assets/icons/birthday-cake.png',
+          headerText: 'Birthday',
+          headerDescription: 'Write a birthday message for a friend',
+          onTap: () {},
+        ),
+
+        // Conversation
+        HomeCardProvider(
+          headerIcon: 'assets/icons/talking.png',
+          headerText: 'Conversation',
+          headerDescription: 'Start a conversation with your AI friend',
+          onTap: () {},
+        ),
+
+        // Invitation
+        HomeCardProvider(
+          headerIcon: 'assets/icons/wedding-invitation.png',
+          headerText: 'Invitation',
+          headerDescription: 'Create an invitation for your event',
+          onTap: () {},
+        ),
+
+        // Speech
+        HomeCardProvider(
+          headerIcon: 'assets/icons/microphone.png',
+          headerText: 'Speech',
+          headerDescription: 'Write a speech for your event',
+          onTap: () {},
+        ),
+
+        // Password Generator
+        HomeCardProvider(
+          headerIcon: 'assets/icons/reset-password.png',
+          headerText: 'Passwords',
+          headerDescription: 'Generate a secure password',
+          onTap: () {},
+        ),
+
+        // Synonyms
+        HomeCardProvider(
+          headerIcon: 'assets/icons/text-box.png',
+          headerText: 'Synonyms',
+          headerDescription: 'Learn new words and their synonyms',
           onTap: () {},
         ),
       ],
@@ -45,7 +137,8 @@ class HomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 10, right: 20),
+                    padding:
+                        const EdgeInsets.only(top: 20, bottom: 10, right: 20),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -62,6 +155,7 @@ class HomePage extends StatelessWidget {
                         TextButton(
                           onPressed: category.onTap,
                           style: TextButton.styleFrom(
+                            fixedSize: const Size(100, 40),
                             foregroundColor: Colors.grey.withOpacity(0.8),
                           ),
                           child: const Text('View All'),
